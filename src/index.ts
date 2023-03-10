f2357481d66d5f37586590f1bafacef9118c2a81
Alexis Mineaud <amineaud@tenable.com>
2023-03-10 23:25:59 +0100

    Expose StoreRoot.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as StoreBase } from './stores/StoreBase'
 export { default as StoreFlags } from './stores/StoreFlags'
+export { default as StoreRoot } from './stores/StoreRoot'
+
+export * from '@/src/stores/types'
