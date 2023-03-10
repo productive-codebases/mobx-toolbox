b766514feacffb54af2dd5b7ad13517f4784f37a
Alexis Mineaud <amineaud@tenable.com>
2023-03-10 23:29:55 +0100

    Remove alias that break exports.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as StoreBase } from './stores/StoreBase'
 export { default as StoreFlags } from './stores/StoreFlags'
 export { default as StoreRoot } from './stores/StoreRoot'
 
-export * from '@/src/stores/types'
+export * from './types'
