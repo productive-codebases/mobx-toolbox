5932ca638d1f80af578798ca23a86e85662ce59e
Alexis Mineaud <amineaud@tenable.com>
2023-03-27 11:20:41 +0200

    Remove unused file.

--- src/libs/newLogger.ts
+++
@@ -1,9 +0,0 @@
-import { setupLogger } from '@productive-codebases/toolbox'
-
-const defaultLoggerMapping = {
-  MobXToolBox: {
-    'stores/StoreRoot': 'stores/StoreRoot'
-  }
-}
-
-export const defaultSetupLogger = setupLogger(defaultLoggerMapping)
